4ab0529cc7bf657aaab9d1f79ea30cfac027a3c1
Peter Cooney <petermcooney@gmail.com>
2024-06-10 18:39:27 +1000

    .

---
+++ peatio-irix/lib/peatio-irix.rb
@@ -0,0 +1,15 @@
+require "peatio"
+#require "faraday_middleware"
+require "faraday"
+require "em-synchrony"
+require "em-websocket"
+require "eventmachine"
+#require "faraday_middleware"
+require "faye"
+
+module Irix
+  require "peatio-irix/bitfinex"
+  require "peatio-irix/huobi"
+  require "peatio-irix/hooks"
+  require "peatio-irix/version"
+end
---
+++ peatio-irix/lib/peatio-irix/version.rb
@@ -0,0 +1,3 @@
+module Irix
+  VERSION = "3.2.1"
+end
